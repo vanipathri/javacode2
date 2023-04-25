d3443a8ee649ee109536e30c367257bee90e37fb
vanipathri <131564884+vanipathri@users.noreply.github.com>
2023-04-25 16:50:14 -0700

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Compare {
+    public  static void compare (int inp1, int inp2) {
+        if (inp1 > inp2) {
+             System.out.println("input 1 is greater than input 2");
+        }
+             else{
+                System.out.println("input 2 is greater than input 1");
+
+    }
+
+
+    }
+    public static void main(String[] args) {
+        compare(20,30);
+    }
+}
