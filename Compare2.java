6058133dcce029ea461174fd77ce882f93fe21a7
vanipathri <131564884+vanipathri@users.noreply.github.com>
2023-04-25 17:28:44 -0700

    Add files via upload

---
+++ Compare2.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Compare2 {
+    public static void compare2(String Str1,String Str2)
+    {
+        if (Str1.equals(Str2)) {
+            System.out.println("String1 equals2 string2");
+        }
+            else{
+                System.out.println("String1 not equals to str2 ");
+            }
+
+    }
+    public static void main(String[] args) {
+        compare2("united states", "india");
+
+    }
+}
